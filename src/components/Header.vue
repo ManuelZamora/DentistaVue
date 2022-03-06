ec9a9723e273f3cb1b9b200f47751884d6e4ee1a
VictorAle123 <vescobedomorales@gmail.com>
2022-03-05 21:46:40 -0600

    se agrego el nabvar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,48 @@
 <template>
-  <div class="header">
+  <div class=" header " id="head" >
     <div class="">
       <div
-        class="antialiased bg-gray-100 dark-mode:bg-gray-900 border border-gray-100 shadow-lg"
+        class="antialiased bg-blue-900 dark-mode:bg-gray-900 border border-gray-100 shadow-lg"
       >
         <div
-          class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800"
+          class="w-full text-white dark-mode:text-gray-200 dark-mode:bg-gray-800"
         >
           <div
             class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
           >
             <div class="flex flex-row items-center justify-between p-4">
+             <div class="flex flex-col w-40 "> 
               <a
                 href="#"
-                class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
-                >Dentista</a
-              >
+                class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+                >Dentista</a>
             </div>
+
+            <div class="flex flex-col w-40 "> 
+              <a
+                href="#"
+                class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+                >citas</a>
+            </div>
+
+            
+            <div class="flex flex-col w-40 "> 
+              <a
+                href="#"
+                class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+                >pacientes</a>
+            </div>
+
+              <div class="flex flex-col w-4 "> 
+              <a
+                href="#"
+                class="text-lg font-semibold tracking-widest text-white uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+                >Informacion</a>
+            </div>
+
+          
+             </div>
+
             <nav
               class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row"
             >
@@ -32,7 +58,7 @@
                 ></span>
                 <span
                   v-else
-                  class="absolute bottom-0 right-0 inline-block w-3 h-3 bg-red-600 border-2 border-white rounded-full"
+                  class="absolute bottom-0 right-0 inline-block w-3 h-3 bg-gray-600 border-2 border-white rounded-full"
                 ></span>
               </div>
 
@@ -48,7 +74,26 @@
       </div>
     </div>
   </div>
+
+
+
+
 </template>
+
+
+  <style>
+
+    
+      body{
+
+        background-color:rgba(221, 27, 27, 0.616);
+
+      }
+    
+
+
+    </style>
+
 
 <script>
 import { onBeforeMount, reactive, toRefs } from "vue";
